b1aa5cef4495938312377a238e12f0c1f2fc9b2c
Craig L. Ching <craigching@gmail.com>
2014-05-11 14:25:16 -0500

    * Missed this in previous commit

--- plot1.R
+++ plot1.R
@@ -2,9 +2,11 @@ source("./loadData.R")
 
 data <- loadData()
 
+png("plot1.png")
+
 hist(data$Global_active_power,
      main = "Global Active Power",
      col = "red",
      xlab = "Global Active Power (kilowatts)")
-dev.copy(png, file = "plot1.png")
+#dev.copy(png, file = "plot1.png")
 dev.off()
